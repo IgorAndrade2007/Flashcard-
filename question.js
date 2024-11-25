e2aebfa600f8cfeffe0cd4a05a47393633193467
IgorAndrade2007 <igor.marcarian@escola.pr.gov.br>
2024-11-25 09:02:51 -0300

    hibm

---
+++ question.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é moto? 
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do Brasil é Brasília 
+    )
+
+criaCartao(
+    'Culinária',
+    'Quais são os pratos típicos do Brasil?',
+    'Os pratos mais típicos da Brasil são, churrasco, pão de queijo, açaí e feijoada 
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz cartão em Inglês?',
+    'cartão em ingles é card 
+)
